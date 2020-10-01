115a75470c893ac24a4a902a81f6c8a08ae7e6f2
Anton <amiller@yandex.ru>
2020-10-01 17:23:48 +1100

    upd 5

--- maven/cmd.sh
+++ maven/cmd.sh
@@ -4,4 +4,4 @@ cd /tmp
 
 git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git \
     && mvn package -f /tmp/boxfuse-sample-java-war-hello/pom.xml \
-    && cp /tmp/boxfuse-sample-java-war-hello/target/hello-1.0/* /artifact
+    && cp -r /tmp/boxfuse-sample-java-war-hello/target/hello-1.0/* /artifact
