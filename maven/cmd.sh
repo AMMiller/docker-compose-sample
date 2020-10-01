90cf69ec70247b187a734c449d121837b99f275e
Anton <anton@localhost.localdomain>
2020-10-01 16:44:34 +1100

    initial commit

---
+++ maven/cmd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /tmp
+
+git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git \
+    && mvn package -f /tmp/boxfuse-sample-java-war-hello/pom.xml \
+    && cp /tmp/boxfuse-sample-java-war-hello/target/hello-1.0/* /artifact
